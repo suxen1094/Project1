4fd1368437908a30fb6cf99ddac49ae95461460f
志翔 蘇 <suxen1094@gmail.com>
2020-10-13 19:25:28 +0800

    test

---
+++ Project1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	int m, n;
+	string tmp;
+	char cc[256];
+	ifstream in;
+	ofstream out;
+	in.open("1.data");
+
+	while (!in.eof()) {
+		in.get
+	}
+	return 0;
+}
